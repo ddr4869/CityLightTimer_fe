f5b87535619646a10bf24ecc4193e503e073ed22
ddr4869 <bikdu@naver.com>
2024-01-21 16:59:54 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,86 @@
+import { useEffect } from 'react';
+import $ from 'jquery';
+import Light from'./traffic_light';
+//import axios from 'axios';
+
+
+const IndexPage = () => {
+  useEffect(() => {
+    
+      try {
+        //const geojson = require('./geo.json')
+        // const response = await axios.get('./geo.json');
+        // const geojson = response.data;
+
+        // 네이버 지도 API 스크립트 동적으로 로드
+        const script = document.createElement('script');
+        script.src = 'https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=e27tyis8vn';
+        script.async = true;
+        document.head.appendChild(script);
+
+        // 스크립트 로드 후 콜백 등록
+        script.onload = () => {
+          // 네이버 지도 초기화 및 표시
+          const mapOptions = {
+            center: new window.naver.maps.LatLng(37.5560943, 126.9342876),
+            //center: new window.naver.maps.LatLng(37.5995239, 126.9165965),
+            zoom: 18,
+          };
+
+          const map = new window.naver.maps.Map('map', mapOptions);
+
+          // 마커 추가 예시
+          const marker = new window.naver.maps.Marker({
+            position: new window.naver.maps.LatLng(37.5560943, 126.9342876),
+            map: map,
+            icon: {
+              url:"light.png",
+              size: new naver.maps.Size(500, 52),
+              scaledSize: new naver.maps.Size(40,40),
+              origin: new naver.maps.Point(0, 0),
+              anchor: new naver.maps.Point(25, 26)
+            }
+          });
+
+            // 클릭시 정보 창 예제
+            var markerList = [];
+            var menuLayer = $('<div style="position:absolute;z-index:10000;background-color:#fff;border:solid 1px #333;padding:10px;display:none;"></div>');
+            map.getPanes().floatPane.appendChild(menuLayer[0]);
+          
+            marker.addListener('click', function(e) {
+              var coordHtml =
+                  'Coord: '+ '(우 클릭 지점 위/경도 좌표)' + '<br />' +
+                  'Point: ' + e.point + '<br />' +
+                  'Offset: ' + e.offset;
+              
+              menuLayer.show().css({  
+                position: "absolute",
+                // left: marker.getPosition().x + "px", // x좌표
+                // top: marker.getPosition().y + "px", // y좌표
+                textAlign: "center",
+                left: e.offset.x,
+                top: e.offset.y,
+              }).html(coordHtml);
+              
+              console.log('Coord: ' + e.coord.toString());
+              
+          });
+        };
+
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+  }, []);
+
+  return (
+    <div>
+      <h1>귀요미 더스틴</h1>
+      <div id="map" style={{ width: '100%', height: '1000px' }}></div>
+      <Light></Light>
+    </div>
+  );
+};
+export default function Page() {
+  return <IndexPage />;
+  //return <h1>Hello, Next.js!</h1>
+}
---
+++ pages/traffic_light.tsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react';
+import $ from 'jquery';
+
+function Light() {
+//const Light = () => {
+    const [response, setResponse] = useState('')
+
+    useEffect(() => {
+        // jQuery를 사용하여 Ajax 요청 보내기
+        $.ajax({
+          url: 'http://localhost:8080/light', // 요청 보낼 URL
+          type: 'GET', // HTTP 메소드
+          dataType: 'text', // 응답 데이터 타입
+          data: {
+            apiKey: 'b2a72569-9548-4d0d-bd01-8fca5d2740df',
+            itstId: '22904',
+            pageNo: '1',
+            numOfRows: '1'
+          },
+          success: function(resp) {
+            // 성공적으로 응답 받았을 때의 처리
+            setResponse(resp);
+            console.log('Response from server:', resp);
+          },
+          error: function(error) {
+            // 에러 발생 시의 처리
+            console.error('Error during Ajax request:', error);
+          }
+        });
+      }, []);
+    
+      return (
+
+        <div id="resp" style={{ wordWrap: 'break-word' }}>
+            <h1>Response from Server</h1>
+            <p>{response}</p>
+        </div>
+      );
+  };
+  
+  //export default Light;
+  export default Light;
